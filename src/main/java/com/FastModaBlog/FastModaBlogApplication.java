a80c6f2e30c81e9371b8e1e61c2671aa34faa07f
Isaac <morabisaac@gmail.com>
2024-02-04 18:58:18 -0600

    commit inicial

---
+++ src/main/java/com/FastModaBlog/FastModaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.FastModaBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastModaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastModaBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FastModaBlog/FastModaBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FastModaBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastModaBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
